78484eef992578b4f18392670bc8f911ce2a9788
Shubham Paul <paulplusx@gmail.com>
2016-11-07 18:49:31 +0530

    update gplv3

--- RTPT/trajectory.ino
+++ RTPT/trajectory.ino
@@ -1,3 +1,18 @@
+/*
+RTPT (Real Time Planet Tracking System and Trajectory Prediction)
+Copyright © 2016  Shubham Paul , Samhita Ganguly ,Rohit Kumar
+This file is part of RTPT.
+    RTPT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    RTPT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with RTPT.  If not, see <http://www.gnu.org/licenses/>.
+ */
 int trajec()
 {
   hh=0;
