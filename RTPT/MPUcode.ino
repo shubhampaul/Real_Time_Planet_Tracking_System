c1258ffcb204c7011dfbfa66a225c35cda5bb8c3
Shubham Paul <paulplusx@gmail.com>
2016-11-07 18:46:10 +0530

    update gplv3

--- RTPT/MPUcode.ino
+++ RTPT/MPUcode.ino
@@ -1,3 +1,23 @@
+/*
+RTPT (Real Time Planet Tracking System and Trajectory Prediction)
+Copyright © 2016  Shubham Paul , Samhita Ganguly ,Rohit Kumar
+This file is part of RTPT.
+    RTPT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    RTPT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with RTPT.  If not, see <http://www.gnu.org/licenses/>.
+ MPU9250 Basic Example Code
+ by: Kris Winer
+ date: April 1, 2014
+ license: Beerware - Use this code however you'd like. If you 
+ find it useful you can buy me a beer some time.
+ */
 void MPUloop()
 {
   //Serial.println("A");
